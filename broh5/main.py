59fbcd0c57244e21da660717424f1ccdbcdd7884
nghia-vo <nvo@bnl.gov>
2023-11-29 16:35:33 -0500

    Add option to specify port

--- broh5/main.py
+++ broh5/main.py
@@ -1,9 +1,11 @@
 import os
+import sys
+import signal
+import socket
 import argparse
-from nicegui import ui
+from nicegui import ui, app
 from broh5.lib.interactions import GuiInteraction
 from broh5 import __version__
-
 
 display_msg = """
 ===============================================================================
@@ -19,22 +21,47 @@ Exit the software by pressing: Ctrl + C
 """
 
 
+def handle_shutdown():
+    print("\n===============")
+    print(" Exit the app!")
+    print("===============\n")
+
+
+def signal_handler(*args):
+    handle_shutdown()
+    sys.exit(0)
+
+
+def check_port(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description=display_msg,
                                      formatter_class=
                                      argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--version', action='version',
-                        version=f'%(prog)s {__version__}')
-    parser.parse_args()
+    parser.add_argument("--version", action="version",
+                        version=f"%(prog)s {__version__}")
+    parser.add_argument("--port", type=int, default=8180,
+                        help="Specify the port to run broh5 on "
+                             "(default: 8180)")
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    parse_args()
+    signal.signal(signal.SIGINT, signal_handler)
+    args = parse_args()
+    if check_port(args.port):
+        print("\n!!! Port {} is already in use. Please specify a "
+              "different port using --port !!!\n".format(args.port))
+        sys.exit(1)
     GuiInteraction()
     os.environ["NO_NETIFACES"] = "True"
-    ui.run(reload=False, title="Browser-based Hdf Viewer", port=8110)
+    app.on_shutdown(handle_shutdown)
+    ui.run(reload=False, title="Browser-based Hdf Viewer", port=args.port)
 
 
 if __name__ in {"__main__", "__mp_main__"}:
     main()
-
